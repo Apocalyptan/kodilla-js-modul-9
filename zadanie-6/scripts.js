9a1890992070fac275d1c521b2740e24495de7f7
Adam Rudny <rudnyadam@gmail.com>
2017-04-29 13:53:43 +0200

    Add Zadanie 9.6

---
+++ zadanie-6/scripts.js
@@ -0,0 +1,9 @@
+var list = document.getElementById("list");
+var add = document.getElementById("addElem");
+var n = list.getElementsByTagName("li");
+
+add.addEventListener("click", function () {
+	var createNewItem = document.createElement("li");
+	var itemNumber = n.length
+	list.appendChild(createNewItem).innerHTML += 'item ' + itemNumber;
+})
