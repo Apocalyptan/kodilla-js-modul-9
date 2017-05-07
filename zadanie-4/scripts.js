4209f727ded753efb7722af890de29464002db7d
Adam Rudny <rudnyadam@gmail.com>
2017-05-07 17:56:02 +0200

    Add Zadanie 9.4 changes

--- zadanie-4/scripts.js
+++ zadanie-4/scripts.js
@@ -2,15 +2,14 @@ function drawTree(treeHeight) {
 	for (var i = 0; i < treeHeight; i++) {
 		var star = '';
 		var whitespace = '';
-		for (var j = 0; j < i + 1; j++) {
+		for (var j = 0; j < 2 * i + 1; j++) {
 			star += '*';
 		}
-		// for (var k = 0; k < i + 1; k++) {
-		// 	whitespace += " ";
-		// }
+		for (var k = i; k < treeHeight; k++) {
+			whitespace += " ";
+		}
 		console.log(whitespace + star);
 	}
 }
 
-drawTree(5);
-//drawTree(prompt('Wpisz wysokość choinki:'));
+drawTree(prompt('Wpisz wysokość choinki:'));
